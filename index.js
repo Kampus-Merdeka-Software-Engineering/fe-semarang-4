27ceaaa585f460c39116b216033717f74f0fa3fa
vincalaudia <claudiajovinca@gmail.com>
2023-09-23 03:37:11 +0700

    Added Catalog and About Us

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 // Toggle class active
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
-}
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+var prevScrollPos = window.pageYOffset;
 
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
-}) 
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos < currentScrollPos) {
+    // User is scrolling up, show the navbar
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    // User is scrolling down, hide the navbar
+    document.querySelector(".navbar").style.top = "-90px";
+  }
+
+  prevScrollPos = currentScrollPos;
+};
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
